94d147a3295020b4d5d9a2953bd9f58db3db313f
Witold <witoldmateusznypel@gmail.com>
2024-07-25 18:42:58 +0200

    - Added button to generate new grid of divs and the whole alrgothm to it.

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const containerDiv = document.querySelector('.container');
+const btnGenerate = document.querySelector('.btn-gen');
+
 function createDivSquare() {
     let html = '';
     for (let i = 0; i < 256; i++) { // 16x16 = 256
@@ -8,3 +10,16 @@ function createDivSquare() {
 }
 
 createDivSquare();
+
+btnGenerate.addEventListener('click', () => {
+    const rowNumsByUser = prompt('Please, enter number of rows');
+    const grid = rowNumsByUser * rowNumsByUser;
+    function createDivSquare(rows) {
+        let html = '';
+        for (let i = 0; i < rows; i++) { // 16x16 = 256
+            html += '<div class="columns"></div>';
+        }
+        containerDiv.innerHTML = html;
+    }
+    createDivSquare(grid);
+});
